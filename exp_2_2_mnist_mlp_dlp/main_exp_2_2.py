9dfc5a68f8619646e3a22f10733a350e1db6f8ac
L. Jiang <l.jiang.1024@gmail.com>
2020-12-31 15:42:08 +0800

    update

--- exp_2_2_mnist_mlp_dlp/main_exp_2_2.py
+++ exp_2_2_mnist_mlp_dlp/main_exp_2_2.py
@@ -3,23 +3,24 @@ import test_cpu
 import time
 import numpy as np
 
+
 def evaluate(mlp):
     pred_results = np.zeros([mlp.test_data.shape[0]])
-    for idx in range(mlp.test_data.shape[0]/mlp.batch_size):
+    for idx in range(mlp.test_data.shape[0] / mlp.batch_size):
         # print("batch %d"%idx)
-        batch_images = mlp.test_data[idx*mlp.batch_size:(idx+1)*mlp.batch_size, :-1]
+        batch_images = mlp.test_data[idx * mlp.batch_size:(idx + 1) * mlp.batch_size, :-1]
         data = batch_images.flatten().tolist()
         mlp.net.setInputData(data)
         start = time.time()
         mlp.forward()
         end = time.time()
-        print('inferencing time: %f'%(end - start))
+        print('inferencing time: %f' % (end - start))
         prob = mlp.net.getOutputData()
         prob = np.array(prob).reshape((mlp.batch_size, mlp.out_classes))
         pred_labels = np.argmax(prob, axis=1)
-        pred_results[idx*mlp.batch_size:(idx+1)*mlp.batch_size] = pred_labels
-    if mlp.test_data.shape[0] % mlp.batch_size >0: 
-        last_batch = mlp.test_data.shape[0]/mlp.batch_size*mlp.batch_size
+        pred_results[idx * mlp.batch_size:(idx + 1) * mlp.batch_size] = pred_labels
+    if mlp.test_data.shape[0] % mlp.batch_size > 0:
+        last_batch = mlp.test_data.shape[0] / mlp.batch_size * mlp.batch_size
         batch_images = mlp.test_data[-last_batch:, :-1]
         data = batch_images.flatten().tolist()
         mlp.net.setInputData(data)
@@ -27,14 +28,15 @@ def evaluate(mlp):
         prob = mlp.net.getOutputData()
         pred_labels = np.argmax(prob, axis=1)
         pred_results[-last_batch:] = pred_labels
-    accuracy = np.mean(pred_results == mlp.test_data[:,-1])
+    accuracy = np.mean(pred_results == mlp.test_data[:, -1])
     print('Accuracy in test set: %f' % accuracy)
+
 
 def run_mnist():
     batch_size = 10000
     h1, h2, c = HIDDEN1, HIDDEN2, OUT
     mlp = MNIST_MLP()
-    mlp.build_model(batch_size=batch_size, hidden1=h1, hidden2=h2, out_classes=c, 
+    mlp.build_model(batch_size=batch_size, hidden1=h1, hidden2=h2, out_classes=c,
                     quant_param_path='../mnist_mlp_quant_param.npz')
     model_path = 'stu_upload/weight.npy'
     test_data = '../mnist_data/t10k-images-idx3-ubyte'
@@ -44,6 +46,7 @@ def run_mnist():
 
     for i in range(10):
         evaluate(mlp)
+
 
 if __name__ == '__main__':
     print('-------- TEST CPU --------')
